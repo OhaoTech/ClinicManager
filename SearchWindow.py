fab80eb0ff16b25143cbb62644c7dab5292c8f00
FrankYin <djmax96945147@outlook.com>
2023-08-03 18:06:02 +0800

    constructor parameter fixed

--- SearchWindow.py
+++ SearchWindow.py
@@ -5,8 +5,9 @@ from PySide6 import QtWidgets
 from ui_searchWindow import Ui_SearchWindow
 
 class SearchWindow(QtWidgets.QMainWindow):
-    def __init__(self, parent=None):
+    def __init__(self, parent, database):
         super().__init__(parent)
         self.ui = Ui_SearchWindow()
         self.ui.setupUi(self)
+        self.database = database
 
