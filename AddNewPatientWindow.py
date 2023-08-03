6fa2d22faaff47aa9d90a9bfaa5184f08b2545c1
FrankYin <djmax96945147@outlook.com>
2023-08-04 00:37:17 +0800

    0.1 version: base function finished
    add new patients as 1st time outpatient service

--- AddNewPatientWindow.py
+++ AddNewPatientWindow.py
@@ -1,35 +1,45 @@
 # This Python file uses the following encoding: utf-8
-from PySide6 import QtCore
+from PySide6.QtCore import QDateTime
 from PySide6 import QtWidgets
-from PySide6.QtWidgets import QWidget, QLabel, QTextEdit
+from PySide6.QtWidgets import QWidget, QLabel, QTextEdit, QMessageBox
 from PySide6.QtGui import QPalette, QColor
+
 
 from ui_addNewPatientWindow import Ui_AddNewPatientWindow
 
 import re
 
+from Database import Database
+
 
 class AddNewPatientWindow(QtWidgets.QMainWindow):
-    def __init__(self, parent, database):
+    def __init__(self, parent, database = Database | None):
         super().__init__(parent)
 
         self.ui = Ui_AddNewPatientWindow()
         self.ui.setupUi(self)
         self.initStyle()
         
+        #push button
         self.ui.cancel_pushButton.clicked.connect(self.closeWindow)
         self.ui.ok_pushButton.clicked.connect(self.saveInfo)
+        
         self.database = database
         
+        #error label
         self.error_label = QLabel(self)
         self.error_label.setStyleSheet("color:red")
-        self.error_label.setGeometry(-100, -100,0,0)
-        self.error_label.setText("NULL")
         
+        #focus out event
         self.ui.name_textEdit.focusOutEvent = self.name_txtEdit_focus_out_event
         self.ui.tel_textEdit.focusOutEvent = self.tel_txtEdit_focus_out_event
+        self.ui.gender_comboBox.focusOutEvent = self.gender_comboBox_focus_out_event
+        
+        #current date time  
+        self.ui.add_dateTimeEdit.setDateTime(QDateTime.currentDateTime())
         
     def initStyle(self):
+        self.ui.remark_textEdit.setPlaceholderText("None")
         self.ui.allergic_history_textEdit.setPlaceholderText("None")
         self.ui.past_medical_history_textEdit.setPlaceholderText("None")
         self.ui.chief_complaint_textEdit.setPlaceholderText("None")
@@ -37,6 +47,7 @@ class AddNewPatientWindow(QtWidgets.QMainWindow):
         self.ui.examinination_textEdit.setPlaceholderText("None")
         self.ui.diagnosis_textEdit.setPlaceholderText("None")
         self.ui.remedy_textEdit.setPlaceholderText("None")
+        
 
         
     def closeWindow(self):
@@ -63,9 +74,33 @@ class AddNewPatientWindow(QtWidgets.QMainWindow):
         else:
             self.error_label.setText("")
             QTextEdit.focusOutEvent(self.ui.name_textEdit, event)
-            
+
+    def gender_comboBox_focus_out_event(self, event):
+        self.error_label.setText("")
+        QTextEdit.focusOutEvent(self.ui.name_textEdit, event)
+        
+
+
+    def show_confirmation_dialog(self) -> bool:
+        formatted_date_time = self.ui.add_dateTimeEdit.dateTime().toString("yyyy-MM-dd HH:mm:ss")
+        confirm_dialog = QMessageBox(self)
+        confirm_dialog.setIcon(QMessageBox.Warning)
+        confirm_dialog.setWindowTitle('Confirmation')
+        confirm_dialog.setText(f'Proceed with date and time: {formatted_date_time}?')
+        confirm_dialog.setStandardButtons(QMessageBox.Yes | QMessageBox.Cancel)
+        confirm_dialog.setDefaultButton(QMessageBox.Cancel)
+
+        response = confirm_dialog.exec_()
+        if response == QMessageBox.Yes:
+            return True            
+        else:
+            return False
+
 
     def saveInfo(self):
+        if not self.show_confirmation_dialog():
+            return
+        #patient table
         name = self.ui.name_textEdit.toPlainText()
         gender = self.ui.gender_comboBox.currentText()
         birthdate = self.ui.birth_dateEdit.date().toString()
@@ -77,9 +112,34 @@ class AddNewPatientWindow(QtWidgets.QMainWindow):
 
         if self.error_label.text() != "":
             pass
+        elif self.ui.gender_comboBox.currentText() == "(Select)":
+            coord = self.ui.gender_comboBox.geometry().bottomLeft()
+            self.error_label.setGeometry(coord.x(), coord.y(), 300, 11)
+            self.error_label.setText("Please select gender")
+        elif self.ui.name_textEdit.toPlainText() == "":
+            coord = self.ui.name_textEdit.geometry().bottomLeft()
+            self.error_label.setGeometry(coord.x(), coord.y(), 300, 11)
+            self.error_label.setText("Invalid Name")   
+        elif self.ui.tel_textEdit.toPlainText() == "":
+            coord = self.ui.tel_textEdit.geometry().bottomLeft()
+            self.error_label.setGeometry(coord.x(), coord.y(), 300, 11)
+            self.error_label.setText("Invalid TEL number")   
         else:
-            self.database.insert_patient(name, gender, birthdate, tel, address, remark, allergic_history, past_medical_history)
-            self.close()
+            patient_id = self.database.insert_patient(name, gender, birthdate, tel, address, remark, allergic_history, past_medical_history)
+                    
+        #visit table
+        visit_date = self.ui.add_dateTimeEdit.dateTime().toString("yyyy-MM-dd HH:mm:ss")
+        cheif_complaint = self.ui.chief_complaint_textEdit.toPlainText()
+        present_illness = self.ui.history_of_the_present_illness_textEdit.toPlainText()
+        visit_id = self.database.insert_visit(patient_id, visit_date, cheif_complaint, present_illness)
+        
+        #logs table
+        examination = self.ui.examinination_textEdit.toPlainText()
+        diagnosis = self.ui.diagnosis_textEdit.toPlainText()
+        remedy = self.ui.remedy_textEdit.toPlainText()
+        self.database.insert_log(visit_id, examination, diagnosis, remedy)
+        
+        self.close()
         
         
 
--- ui_addNewPatientWindow.py
+++ ui_addNewPatientWindow.py
@@ -15,10 +15,10 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QComboBox, QDateEdit, QDialog,
-    QFrame, QGroupBox, QLabel, QPushButton,
-    QScrollArea, QScrollBar, QSizePolicy, QTextEdit,
-    QWidget)
+from PySide6.QtWidgets import (QAbstractSpinBox, QApplication, QComboBox, QDateEdit,
+    QDateTimeEdit, QDialog, QFrame, QGroupBox,
+    QLabel, QPushButton, QScrollArea, QScrollBar,
+    QSizePolicy, QTextEdit, QWidget)
 
 class Ui_AddNewPatientWindow(object):
     def setupUi(self, AddNewPatientWindow):
@@ -45,24 +45,24 @@ class Ui_AddNewPatientWindow(object):
         self.groupBox.setGeometry(QRect(0, 0, 880, 200))
         self.name_textEdit = QTextEdit(self.groupBox)
         self.name_textEdit.setObjectName(u"name_textEdit")
-        self.name_textEdit.setGeometry(QRect(60, 60, 131, 31))
+        self.name_textEdit.setGeometry(QRect(60, 60, 91, 31))
         self.name_textEdit.setStyleSheet(u"background-color: rgb(192, 191, 188);")
         self.tel_label = QLabel(self.groupBox)
         self.tel_label.setObjectName(u"tel_label")
         self.tel_label.setGeometry(QRect(10, 110, 51, 21))
         self.birthdata_label = QLabel(self.groupBox)
         self.birthdata_label.setObjectName(u"birthdata_label")
-        self.birthdata_label.setGeometry(QRect(410, 60, 71, 21))
+        self.birthdata_label.setGeometry(QRect(370, 60, 71, 21))
         self.remark_textEdit = QTextEdit(self.groupBox)
         self.remark_textEdit.setObjectName(u"remark_textEdit")
-        self.remark_textEdit.setGeometry(QRect(80, 150, 251, 31))
+        self.remark_textEdit.setGeometry(QRect(680, 60, 181, 31))
         self.remark_textEdit.setStyleSheet(u"background-color: rgb(192, 191, 188);")
         self.address_label = QLabel(self.groupBox)
         self.address_label.setObjectName(u"address_label")
-        self.address_label.setGeometry(QRect(310, 110, 111, 31))
+        self.address_label.setGeometry(QRect(210, 110, 111, 31))
         self.birth_dateEdit = QDateEdit(self.groupBox)
         self.birth_dateEdit.setObjectName(u"birth_dateEdit")
-        self.birth_dateEdit.setGeometry(QRect(490, 60, 165, 42))
+        self.birth_dateEdit.setGeometry(QRect(450, 60, 165, 42))
         self.birth_dateEdit.setStyleSheet(u"")
         self.birth_dateEdit.setLocale(QLocale(QLocale.Chinese, QLocale.China))
         self.birth_dateEdit.setCalendarPopup(True)
@@ -71,32 +71,43 @@ class Ui_AddNewPatientWindow(object):
         self.birth_dateEdit.setDate(QDate(1999, 1, 1))
         self.remark_label = QLabel(self.groupBox)
         self.remark_label.setObjectName(u"remark_label")
-        self.remark_label.setGeometry(QRect(10, 150, 61, 21))
+        self.remark_label.setGeometry(QRect(620, 60, 61, 21))
         self.tel_textEdit = QTextEdit(self.groupBox)
         self.tel_textEdit.setObjectName(u"tel_textEdit")
-        self.tel_textEdit.setGeometry(QRect(50, 110, 241, 31))
+        self.tel_textEdit.setGeometry(QRect(50, 110, 151, 31))
         self.tel_textEdit.setStyleSheet(u"background-color: rgb(192, 191, 188);\n"
 "")
         self.gender_label = QLabel(self.groupBox)
         self.gender_label.setObjectName(u"gender_label")
-        self.gender_label.setGeometry(QRect(210, 60, 71, 21))
+        self.gender_label.setGeometry(QRect(170, 60, 71, 21))
         self.name_label = QLabel(self.groupBox)
         self.name_label.setObjectName(u"name_label")
         self.name_label.setGeometry(QRect(10, 60, 51, 21))
         self.address_textEdit = QTextEdit(self.groupBox)
         self.address_textEdit.setObjectName(u"address_textEdit")
-        self.address_textEdit.setGeometry(QRect(420, 110, 331, 71))
+        self.address_textEdit.setGeometry(QRect(320, 110, 541, 31))
         self.address_textEdit.setStyleSheet(u"background-color: rgb(192, 191, 188);")
         self.gender_comboBox = QComboBox(self.groupBox)
         self.gender_comboBox.addItem("")
         self.gender_comboBox.addItem("")
         self.gender_comboBox.addItem("")
         self.gender_comboBox.setObjectName(u"gender_comboBox")
-        self.gender_comboBox.setGeometry(QRect(270, 60, 125, 36))
+        self.gender_comboBox.setGeometry(QRect(230, 60, 125, 36))
         self.gender_comboBox.setStyleSheet(u"")
         self.label = QLabel(self.groupBox)
         self.label.setObjectName(u"label")
         self.label.setGeometry(QRect(10, 20, 141, 31))
+        self.add_time_label = QLabel(self.groupBox)
+        self.add_time_label.setObjectName(u"add_time_label")
+        self.add_time_label.setGeometry(QRect(10, 150, 71, 21))
+        self.add_dateTimeEdit = QDateTimeEdit(self.groupBox)
+        self.add_dateTimeEdit.setObjectName(u"add_dateTimeEdit")
+        self.add_dateTimeEdit.setGeometry(QRect(80, 150, 226, 42))
+        self.add_dateTimeEdit.setLocale(QLocale(QLocale.Chinese, QLocale.China))
+        self.add_dateTimeEdit.setButtonSymbols(QAbstractSpinBox.UpDownArrows)
+        self.add_dateTimeEdit.setDateTime(QDateTime(QDate(2000, 1, 1), QTime(8, 0, 0)))
+        self.add_dateTimeEdit.setCalendarPopup(False)
+        self.add_dateTimeEdit.setTimeSpec(Qt.LocalTime)
         self.groupBox_2 = QGroupBox(self.scrollAreaWidgetContents)
         self.groupBox_2.setObjectName(u"groupBox_2")
         self.groupBox_2.setGeometry(QRect(0, 200, 880, 240))
@@ -197,6 +208,14 @@ class Ui_AddNewPatientWindow(object):
         self.address_label.setText(QCoreApplication.translate("AddNewPatientWindow", u"Home Address:", None))
         self.birth_dateEdit.setDisplayFormat(QCoreApplication.translate("AddNewPatientWindow", u"yyyy/MM/dd", None))
         self.remark_label.setText(QCoreApplication.translate("AddNewPatientWindow", u"Remark:", None))
+        self.tel_textEdit.setHtml(QCoreApplication.translate("AddNewPatientWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"hr { height: 1px; border-width: 0; }\n"
+"li.unchecked::marker { content: \"\\2610\"; }\n"
+"li.checked::marker { content: \"\\2612\"; }\n"
+"</style></head><body style=\" font-family:'Cantarell'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>", None))
         self.gender_label.setText(QCoreApplication.translate("AddNewPatientWindow", u"Gender:", None))
         self.name_label.setText(QCoreApplication.translate("AddNewPatientWindow", u"Name:", None))
         self.gender_comboBox.setItemText(0, QCoreApplication.translate("AddNewPatientWindow", u"(Select)", None))
@@ -204,6 +223,8 @@ class Ui_AddNewPatientWindow(object):
         self.gender_comboBox.setItemText(2, QCoreApplication.translate("AddNewPatientWindow", u"Female", None))
 
         self.label.setText(QCoreApplication.translate("AddNewPatientWindow", u"<html><head/><body><p><span style=\" font-size:18pt; font-weight:700;\">Patient Info</span></p></body></html>", None))
+        self.add_time_label.setText(QCoreApplication.translate("AddNewPatientWindow", u"Add time:", None))
+        self.add_dateTimeEdit.setDisplayFormat(QCoreApplication.translate("AddNewPatientWindow", u"yyyy/MM/dd HH:mm:ss\u202f", None))
         self.groupBox_2.setTitle("")
         self.medical_history_label.setText(QCoreApplication.translate("AddNewPatientWindow", u"<html><head/><body><p><span style=\" font-size:18pt; font-weight:700;\">Medical History</span></p></body></html>", None))
         self.allergic_history_label.setText(QCoreApplication.translate("AddNewPatientWindow", u"Allergic History: ", None))
