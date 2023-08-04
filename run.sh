c4e4a49a8159688ffd2918db37fefcd4abda4d5a
FrankYin <djmax96945147@outlook.com>
2023-08-04 17:52:50 +0800

    add new file

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+pyside6-uic PatientInfoWindow.ui -o ui_PatientInfoWindow.py
 pyside6-uic addNewPatientWindow.ui -o ui_addNewPatientWindow.py
 pyside6-uic searchWindow.ui -o ui_searchWindow.py
 pyside6-uic form.ui -o ui_form.py
