6641a75b77f24957ff793682e7f83045b103bf00
FrankYin <djmax96945147@outlook.com>
2023-08-07 18:00:04 +0800

    Create language.ts

---
+++ language.ts
@@ -0,0 +1,418 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN" sourcelanguage="en_US">
+<context>
+    <name>AddNewPatientWindow</name>
+    <message>
+        <location filename="ui_addNewPatientWindow.py" line="202"/>
+        <source>AddNewPatientWindow</source>
+        <translation type="unfinished">添加新患者</translation>
+    </message>
+    <message>
+        <location filename="ui_addNewPatientWindow.py" line="204"/>
+        <source>TEL:</source>
+        <translation type="unfinished">电话：</translation>
+    </message>
+    <message>
+        <location filename="ui_addNewPatientWindow.py" line="205"/>
+        <source>Birth Date:</source>
+        <translation type="unfinished">出生日期：</translation>
+    </message>
+    <message>
+        <location filename="ui_addNewPatientWindow.py" line="206"/>
+        <source>Home Address:</source>
+        <translation type="unfinished">家庭住址：</translation>
+    </message>
+    <message>
+        <location filename="ui_addNewPatientWindow.py" line="207"/>
+        <source>yyyy/MM/dd</source>
+        <translation type="unfinished">年/月/日</translation>
+    </message>
+    <message>
+        <location filename="ui_addNewPatientWindow.py" line="208"/>
+        <source>Remark:</source>
+        <translation type="unfinished">备注：</translation>
+    </message>
+    <message>
+        <location filename="ui_addNewPatientWindow.py" line="209"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;meta charset=&quot;utf-8&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+hr { height: 1px; border-width: 0; }
+li.unchecked::marker { content: &quot;\2610&quot;; }
+li.checked::marker { content: &quot;\2612&quot;; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Cantarell&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_addNewPatientWindow.py" line="217"/>
+        <source>Gender:</source>
+        <translation type="unfinished">性别：</translation>
+    </message>
+    <message>
+        <location filename="ui_addNewPatientWindow.py" line="218"/>
+        <source>Name:</source>
+        <translation type="unfinished">姓名：</translation>
+    </message>
+    <message>
+        <location filename="ui_addNewPatientWindow.py" line="219"/>
+        <source>(Select)</source>
+        <translation type="unfinished">（选择）</translation>
+    </message>
+    <message>
+        <location filename="ui_addNewPatientWindow.py" line="220"/>
+        <source>Male</source>
+        <translation type="unfinished">男</translation>
+    </message>
+    <message>
+        <location filename="ui_addNewPatientWindow.py" line="221"/>
+        <source>Female</source>
+        <translation type="unfinished">女</translation>
+    </message>
+    <message>
+        <location filename="ui_addNewPatientWindow.py" line="223"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:18pt; font-weight:700;&quot;&gt;Patient Info&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_addNewPatientWindow.py" line="224"/>
+        <source>Add time:</source>
+        <translation type="unfinished">添加时间</translation>
+    </message>
+    <message>
+        <location filename="ui_addNewPatientWindow.py" line="225"/>
+        <source>yyyy/MM/dd HH:mm:ssu202f</source>
+        <translation type="unfinished">年/月/日  小时：分钟：秒</translation>
+    </message>
+    <message>
+        <location filename="ui_addNewPatientWindow.py" line="227"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:18pt; font-weight:700;&quot;&gt;Medical History&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_addNewPatientWindow.py" line="228"/>
+        <source>Allergic History: </source>
+        <translation type="unfinished">过敏史</translation>
+    </message>
+    <message>
+        <location filename="ui_addNewPatientWindow.py" line="229"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Past Medical &lt;/p&gt;&lt;p align=&quot;center&quot;&gt;History:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_addNewPatientWindow.py" line="231"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:18pt; font-weight:700;&quot;&gt;Outpatient Records&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_addNewPatientWindow.py" line="232"/>
+        <source>Chief Complaint:</source>
+        <translation type="unfinished">主诉</translation>
+    </message>
+    <message>
+        <location filename="ui_addNewPatientWindow.py" line="233"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;History of the&lt;/p&gt;&lt;p align=&quot;center&quot;&gt; Present &lt;/p&gt;&lt;p align=&quot;center&quot;&gt;Illness:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_addNewPatientWindow.py" line="234"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Examination:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_addNewPatientWindow.py" line="235"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Diagnosis:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_addNewPatientWindow.py" line="236"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Remedy:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_addNewPatientWindow.py" line="237"/>
+        <source>OK</source>
+        <translation type="unfinished">确认</translation>
+    </message>
+    <message>
+        <location filename="ui_addNewPatientWindow.py" line="238"/>
+        <source>Print</source>
+        <translation type="unfinished">打印</translation>
+    </message>
+    <message>
+        <location filename="ui_addNewPatientWindow.py" line="239"/>
+        <source>Cancel</source>
+        <translation type="unfinished">取消</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="ui_form.py" line="77"/>
+        <source>Clinic Manager</source>
+        <translation type="unfinished">诊所管理</translation>
+    </message>
+    <message>
+        <location filename="ui_form.py" line="78"/>
+        <source>Import...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_form.py" line="79"/>
+        <source>Export...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_form.py" line="80"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_form.py" line="81"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:22pt;&quot;&gt;Clinic Manager&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_form.py" line="82"/>
+        <source>Search</source>
+        <translation type="unfinished">搜索</translation>
+    </message>
+    <message>
+        <location filename="ui_form.py" line="83"/>
+        <source>Add New Patient</source>
+        <translation type="unfinished">添加新患者</translation>
+    </message>
+    <message>
+        <location filename="ui_form.py" line="84"/>
+        <source>Export All Patients</source>
+        <translation type="unfinished">导出所有患者数据</translation>
+    </message>
+    <message>
+        <location filename="ui_form.py" line="85"/>
+        <source>Theme</source>
+        <translation type="unfinished">背景主题</translation>
+    </message>
+    <message>
+        <location filename="ui_form.py" line="86"/>
+        <source>auto</source>
+        <translation type="unfinished">自动</translation>
+    </message>
+    <message>
+        <location filename="ui_form.py" line="87"/>
+        <source>light</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_form.py" line="88"/>
+        <source>dark</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PatientInfoWIndow</name>
+    <message>
+        <location filename="ui_PatientInfoWindow.py" line="170"/>
+        <source>PatientInfoWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_PatientInfoWindow.py" line="172"/>
+        <source>Name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_PatientInfoWindow.py" line="173"/>
+        <source>Gender:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_PatientInfoWindow.py" line="175"/>
+        <source>Male</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_PatientInfoWindow.py" line="176"/>
+        <source>Female</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_PatientInfoWindow.py" line="178"/>
+        <source>TEL:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_PatientInfoWindow.py" line="179"/>
+        <source>Remark:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_PatientInfoWindow.py" line="180"/>
+        <source>Home Address:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_PatientInfoWindow.py" line="181"/>
+        <source>Edit Info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_PatientInfoWindow.py" line="182"/>
+        <source>yyyy/MM/dd</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_PatientInfoWindow.py" line="183"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Birthdate:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_PatientInfoWindow.py" line="185"/>
+        <source>Allergic History:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_PatientInfoWindow.py" line="186"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Past Medical &lt;/p&gt;&lt;p align=&quot;center&quot;&gt;History:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_PatientInfoWindow.py" line="187"/>
+        <source>Clear </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_PatientInfoWindow.py" line="188"/>
+        <source>Delete Record</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_PatientInfoWindow.py" line="189"/>
+        <source>Add Record</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_PatientInfoWindow.py" line="190"/>
+        <source>Chief Complaint:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_PatientInfoWindow.py" line="191"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Diagnosis:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_PatientInfoWindow.py" line="192"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;History of the&lt;/p&gt;&lt;p align=&quot;center&quot;&gt; Present &lt;/p&gt;&lt;p align=&quot;center&quot;&gt;Illness:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_PatientInfoWindow.py" line="193"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Examination:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_PatientInfoWindow.py" line="194"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Remedy:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_PatientInfoWindow.py" line="195"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;meta charset=&quot;utf-8&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+hr { height: 1px; border-width: 0; }
+li.unchecked::marker { content: &quot;\2610&quot;; }
+li.checked::marker { content: &quot;\2612&quot;; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Liberation Serif&apos;; font-size:11pt; font-weight:700; font-style:normal;&quot;&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Cantarell&apos;; font-size:22pt;&quot;&gt;Medical Records&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_PatientInfoWindow.py" line="203"/>
+        <source>yyyy-MM-dd HH:mm:ss</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_PatientInfoWindow.py" line="204"/>
+        <source>Realtime:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_PatientInfoWindow.py" line="205"/>
+        <source>Edit Record</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_PatientInfoWindow.py" line="206"/>
+        <source>Delete All Records</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_PatientInfoWindow.py" line="207"/>
+        <source>Export</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SearchWindow</name>
+    <message>
+        <location filename="ui_searchWindow.py" line="89"/>
+        <source>SearchWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_searchWindow.py" line="91"/>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_searchWindow.py" line="92"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:48pt; font-weight:700;&quot;&gt;Personal Info&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_searchWindow.py" line="93"/>
+        <source>Name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_searchWindow.py" line="94"/>
+        <source>TEL:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_searchWindow.py" line="95"/>
+        <source>Gender:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_searchWindow.py" line="96"/>
+        <source>(Select)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_searchWindow.py" line="97"/>
+        <source>Male</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_searchWindow.py" line="98"/>
+        <source>Female</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_searchWindow.py" line="100"/>
+        <source>Delete Selected</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="mainwindow.py" line="62"/>
+        <source>auto</source>
+        <translation type="obsolete">自动</translation>
+    </message>
+</context>
+</TS>
