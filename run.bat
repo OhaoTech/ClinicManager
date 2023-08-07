ea2322b9bc23497574013a7e9c115fb20d3778fd
FrankYin <djmax96945147@outlook.com>
2023-08-07 17:58:48 +0800

    language pack compiles

--- run.bat
+++ run.bat
@@ -2,4 +2,5 @@ pyside6-uic PatientInfoWindow.ui -o ui_PatientInfoWindow.py
 pyside6-uic addNewPatientWindow.ui -o ui_addNewPatientWindow.py
 pyside6-uic searchWindow.ui -o ui_searchWindow.py
 pyside6-uic form.ui -o ui_form.py
+lrelease language.ts
 python mainwindow.py
