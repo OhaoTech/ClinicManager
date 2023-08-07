f3c915ccea2835c88acbaae5e016dce7bf14c59c
FrankYin <djmax96945147@outlook.com>
2023-08-08 00:06:22 +0800

    update Qt Linguist language translation file

--- lang_pack_gen.sh
+++ lang_pack_gen.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-pylupdate5 AddNewPatientWindow.py  Database.py  Exportdatasheet.py  mainwindow.py  PatientInfoWindow.py  SearchWindow.py  ui_addNewPatientWindow.py  ui_form.py  ui_PatientInfoWindow.py  ui_searchWindow.py -ts language.ts
+pylupdate5 *.py -ts language.ts
